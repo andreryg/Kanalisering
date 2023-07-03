fce947ad5bf5bcbeef145e6694b3e34758fe2dfd
Andreas Gangstad Rygg <andreas.gangstad.rygg@vegvesen.no>
2023-07-03 15:47:29 +0200

    html javascript

---
+++ viewer.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  3 10:26:00 2023
+
+@author: andryg
+"""
+
+import webbrowser
+
+
+#index = open("viewer.html").read().format(X="test")
+
+webbrowser.open("viewer.html")
