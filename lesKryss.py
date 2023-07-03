55f3f4f2c55e2b79bbab6bdf10bf6b1fc1860c19
Andreas Gangstad Rygg <andreryg@stud.ntnu.no>
2023-07-03 21:43:20 +0200

    sending coordinates from python

--- lesKryss.py
+++ lesKryss.py
@@ -7,11 +7,19 @@ Created on Mon Jul  3 09:33:07 2023
 
 import nvdbapiv3
 import pandas as pd
+import numpy as np
 
-vegkryss = nvdbapiv3.nvdbFagdata(37)
-vegkryss.filter({'egenskap': '11479="Sommeroppdatering 2023" AND 2080!=3478 AND 2080!=3222 AND 2080!=3226 AND 2080!=3784 AND 1788!=3475 AND 1788!=3219 AND 1788!=3223'})
-vegkryssDF = pd.DataFrame(vegkryss.to_records())
-print(vegkryssDF.columns.values.tolist())
-print(vegkryssDF['geometri'])
+def getData():
+    vegkryss = nvdbapiv3.nvdbFagdata(37)
+    vegkryss.filter({'egenskap': '11479="Sommeroppdatering 2023" AND 2080!=3478 AND 2080!=3222 AND 2080!=3226 AND 2080!=3784 AND 1788!=3475 AND 1788!=3219 AND 1788!=3223'})
+    vegkryssDF = pd.DataFrame(vegkryss.to_records())
+    #print(vegkryssDF.columns.values.tolist())
 
-                 
+    test = [i for i in vegkryssDF['geometri'].apply(lambda x: x.strip("POINTZ()")[3:].split(" "))]
+    
+    fil = open("koordinater.txt", "w")
+    for j in test:
+        j.pop()
+        fil.write(str(list(map(float, j)))+"\n")
+
+getData()
--- viewer.py
+++ viewer.py
@@ -6,8 +6,9 @@ Created on Mon Jul  3 10:26:00 2023
 """
 
 import webbrowser
+from lesKryss import getData
 
 
 #index = open("viewer.html").read().format(X="test")
-
+#getData()
 webbrowser.open("viewer.html")
